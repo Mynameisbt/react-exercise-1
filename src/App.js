0f462d9b40a3fe1546d8ded3cd19b66c61bd6fe5
Brian Thomas <brianjthomas85@gmail.com>
2019-03-23 20:39:32 +0000

    Solution

--- src/App.js
+++ src/App.js
@@ -106,6 +106,13 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+{
+ 	profiles.map((profile) => {
+      const user = users[profile.userID];
+	  const movie = movies[profile.favoriteMovieID];
+      return <div>{`${user.name}'s favorite movie is ${movie.name}`}.</div>;
+    })
+}
       </div>
     );
   }
